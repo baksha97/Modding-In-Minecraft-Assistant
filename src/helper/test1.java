8be0ef87c95fa91d7012a3613e370561acb5ec9d
baksha97 <baksha97@gmail.com>
2017-05-26 00:37:12 -0400

    commit

--- src/helper/test1.java
+++ src/helper/test1.java
@@ -1,7 +1,12 @@
 package helper;
 
+import javaxt.io.Directory;
+
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by loaner on 5/19/17.
@@ -10,36 +15,82 @@ public class test1 {
 
     private static final String FIRE_PRE_LESSON_DIR = "/Minecraft Code/ICE/Pre Lesson Repo";
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        Runtime rt = Runtime.getRuntime();
+       // File s1 = new File("/Users/loaner/Documents/Minecraft/Student 1");
+      // System.out.println(s1.getAbsolutePath()
 
-        System.out.println(File.separator);
-        System.out.println(File.separatorChar);
-        System.out.println(File.pathSeparator);
-        System.out.println(File.pathSeparatorChar);
+          //  Process workspaceSetup = rt.exec( "loaner/bin/bash open Doc*");
+
+        List<String> cmds = new ArrayList<String>();
+        cmds.add("/bin/sh");
+        cmds.add("-c");
+        cmds.add("open Doc*");
+
+        Process workspaceSetup = rt.exec("/bin/bash -c open Doc*");
 
 
-/*
-        File[] fs = getDictionaries(new File("/Users/loaner//Downloads/Minecraft" + FIRE_PRE_LESSON_DIR)); //new File(minecraftFolder.getAbsolutePath() + this.FIRE_PRE_LESSON_DIR).listFiles(File::isDirectory);
 
-        System.out.println(fs);
-        System.out.println(fs.length);
 
-        for(int i=0; i<fs.length; i++){
-            System.out.println(fs[i].getName());
-        }
 
-        System.out.println(fs[0]);
-*/
+
+
+
     }
 
-    private static File[] getDictionaries(File dir){
-        File[] directories = dir.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                return pathname.isDirectory();
+    private static void runGradleSetup(File minecraftFolder){
+
+        Directory t = new Directory(minecraftFolder.getAbsoluteFile());
+
+        String operatingSystem = System.getProperty("os.name");
+/*
+        if(operatingSystem.contains("Windows")) {
+            System.out.println("Operating System: " + operatingSystem);
+            Runtime rt = Runtime.getRuntime();
+            try{
+                System.out.println("STARTING SETUP... -close windows after completion");
+                Process workspaceSetup = rt.exec("cmd.exe /c start /wait gradlew setupDecompWorkspace", null, minecraftFolder);
+                System.out.println("Starting Workspace Setup...");
+                workspaceSetup.waitFor();
+                Process eclipseSetup = rt.exec("cmd.exe /c start /wait gradlew eclipse", null, minecraftFolder);
+                System.out.println("Starting Eclipse Setup...");
+                eclipseSetup.waitFor();
+                System.out.println("... SETUP COMPLETE");
+            }catch(InterruptedException e){
+                System.out.println("Error: Interrupted Exception");
+            }catch(IOException e){
+                System.out.println("Error: IOException Exception");
             }
-        });
-        return directories;
+        }
+*/
+         if(operatingSystem.contains("Mac")){
+            System.out.println("Operating System: " + operatingSystem);
+            Runtime rt = Runtime.getRuntime();
+            try{
+                System.out.println("STARTING SETUP... -close windows after completion");
+
+               //Process workspaceSetup = rt.exec("/bin/bash/ -c ./gradlew setupDecompWorkspace", null, minecraftFolder);
+               // Process workspaceSetup = rt.exec(minecraftFolder.getAbsolutePath() + " ");
+                Process workspaceSetup = rt.exec( minecraftFolder.getAbsolutePath() + "/ ./gradlew setupDecompWorkspace");
+                System.out.println("Starting Workspace Setup...");
+
+
+
+
+                workspaceSetup.waitFor();
+                Process eclipseSetup = rt.exec("cmd.exe /c start /wait gradlew eclipse", null, minecraftFolder);
+                System.out.println("Starting Eclipse Setup...");
+                eclipseSetup.waitFor();
+                System.out.println("... SETUP COMPLETE");
+
+
+
+            }catch(InterruptedException e){
+                System.out.println("Error: Interrupted Exception");
+            }catch(IOException e){
+                System.out.println("Error: IOException Exception");
+            }
+        }
     }
 
 
